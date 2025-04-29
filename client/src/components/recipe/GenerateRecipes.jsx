9864e7e6b4a4316142852282c40b8056ed2eb963
Sanjana de Costa <sanjana.decosta@itelasoft.com>
2025-04-29 10:57:56 +0530

    Sonarqube

---
+++ client/src/components/recipe/GenerateRecipes.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const GenerateRecipes = () => {
+
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default GenerateRecipes;
