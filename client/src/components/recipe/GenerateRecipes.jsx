8ecb1371c197def8229bef6c7885b8731c34d1ef
Sanjana de Costa <sanjana.decosta@itelasoft.com>
2025-05-10 18:12:18 +0530

    Added Generate page

--- client/src/components/recipe/GenerateRecipes.jsx
+++ client/src/components/recipe/GenerateRecipes.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Bell, AlertCircle, Edit, Trash, PlusCircle, Search } from "lucide-react";
 
 const GenerateRecipes = () => {
 
